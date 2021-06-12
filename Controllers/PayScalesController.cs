a69d535f3158c3bd7b4bf022e468aa2059918981
Ikshit <ikshitmagoon99@gmail.com>
2021-06-12 13:40:34 +0530

    done final-commit

---
+++ Controllers/PayScalesController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebAppCoreAzure.Models;
+
+namespace WebAppCoreAzure.Controllers
+{
+    public class PayScalesController : Controller
+    {
+        private readonly PayScaleDbContext _context;
+
+        public PayScalesController(PayScaleDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: PayScales
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.PayScale.ToListAsync());
+        }
+
+        // GET: PayScales/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var payScale = await _context.PayScale
+                .FirstOrDefaultAsync(m => m.EmployeeId == id);
+            if (payScale == null)
+            {
+                return NotFound();
+            }
+
+            return View(payScale);
+        }
+
+        // GET: PayScales/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PayScales/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EmployeeId,PayBand,BasicSalary,Hra,Da,Ta,Tds,NetSalary,InHandSalary")] PayScale payScale)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(payScale);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(payScale);
+        }
+
+        // GET: PayScales/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var payScale = await _context.PayScale.FindAsync(id);
+            if (payScale == null)
+            {
+                return NotFound();
+            }
+            return View(payScale);
+        }
+
+        // POST: PayScales/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("EmployeeId,PayBand,BasicSalary,Hra,Da,Ta,Tds,NetSalary,InHandSalary")] PayScale payScale)
+        {
+            if (id != payScale.EmployeeId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(payScale);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PayScaleExists(payScale.EmployeeId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(payScale);
+        }
+
+        // GET: PayScales/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var payScale = await _context.PayScale
+                .FirstOrDefaultAsync(m => m.EmployeeId == id);
+            if (payScale == null)
+            {
+                return NotFound();
+            }
+
+            return View(payScale);
+        }
+
+        // POST: PayScales/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var payScale = await _context.PayScale.FindAsync(id);
+            _context.PayScale.Remove(payScale);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PayScaleExists(int id)
+        {
+            return _context.PayScale.Any(e => e.EmployeeId == id);
+        }
+    }
+}
---
+++ Models/PayScaleDbContext.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace WebAppCoreAzure.Models
+{
+    public partial class PayScaleDbContext : DbContext
+    {
+        public PayScaleDbContext()
+        {
+        }
+
+        public PayScaleDbContext(DbContextOptions<PayScaleDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<PayScale> PayScale { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=tcp:payscale2.database.windows.net,1433;Initial Catalog=PayScaleDb;Persist Security Info=False;User ID=admin1255;Password=admin@1255;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PayScale>(entity =>
+            {
+                entity.HasKey(e => e.EmployeeId)
+                    .HasName("PK__PayScale__7AD04F11475426D6");
+
+                entity.Property(e => e.Da)
+                    .HasColumnName("DA")
+                    .HasComputedColumnSql("((0.05)*[BasicSalary])");
+
+                entity.Property(e => e.Hra)
+                    .HasColumnName("HRA")
+                    .HasComputedColumnSql("((0.1)*[BasicSalary])");
+
+                entity.Property(e => e.InHandSalary).HasComputedColumnSql("(((([BasicSalary]+(0.1)*[BasicSalary])+(0.05)*[BasicSalary])+(0.05)*[BasicSalary])-(0.1)*[BasicSalary])");
+
+                entity.Property(e => e.NetSalary)
+                    .HasColumnName("netSalary")
+                    .HasComputedColumnSql("((([BasicSalary]+(0.1)*[BasicSalary])+(0.05)*[BasicSalary])+(0.05)*[BasicSalary])");
+
+                entity.Property(e => e.PayBand)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Ta)
+                    .HasColumnName("TA")
+                    .HasComputedColumnSql("((0.05)*[BasicSalary])");
+
+                entity.Property(e => e.Tds)
+                    .HasColumnName("TDS")
+                    .HasComputedColumnSql("((0.1)*[BasicSalary])");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
